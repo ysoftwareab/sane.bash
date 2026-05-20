12ee8ab24ef6209e8685aec240a655c3926d82ba
Andrei Neculau <andrei.neculau@gmail.com>
2026-05-20 09:03:06 +0200

    fixup! add failing command

--- docs-sane.bash
+++ docs-sane.bash
@@ -15,7 +15,7 @@ export PS4='+(${BASH_SOURCE[0]}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 # Show stack trace on error
 # shellcheck disable=SC2034,SC2154
-trap 'e=$?; set +x; s() { cmd=$BASH_COMMAND; >&2 echo "bash: *** [command] ${cmd}"; local i=0; while caller $i; do ((++i)); done | while read l f p; do echo "bash: *** [$p:$l $f]${e:+ Error }$e"; e=""; done; }; >&2 echo; >&2 s' ERR # editorconfig-checker-disable-line
+trap 'e=$?; set +x; s() { cmd="${BASH_COMMAND}"; >&2 echo "bash: *** [command] ${cmd}"; local i=0; while caller $i; do ((++i)); done | while read l f p; do echo "bash: *** [$p:$l $f]${e:+ Error }$e"; e=""; done; }; >&2 echo; >&2 s' ERR # editorconfig-checker-disable-line
 
 # Helper function for logging handpicked commands before executing them
 function exe() { printf '%s\n' "$(pwd)\$ $(printf '%q ' "$@")" >&2 && "$@"; }
